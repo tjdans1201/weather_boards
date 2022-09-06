20e3f25e8beb8f6a198b173df7b44a5bad6245b4
Seongmun <tjdans1201@gmail.com>
2022-09-06 13:53:02 +0900

    Feat : 게시물 삭제 API 추가

--- boards/urls.py
+++ boards/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("get_board_list/", views.get_board_list),
     path("get_board_detail/<int:id>/", views.get_board_detail),
     path("create_board/", views.create_board),
+    path("delete_board/<int:id>/", views.delete_board)
 ]
--- boards/views.py
+++ boards/views.py
@@ -52,7 +52,6 @@ def create_board(request):
     """
     게시물을 작성한다.
     데이터 생성 시 현재 날씨를 외부 API로부터 취득하여 추가한다.
-    Todo: password 암호화
     """
     try:
         request_body = request.data
@@ -62,7 +61,7 @@ def create_board(request):
             return Response({"message":message}, status=status.HTTP_400_BAD_REQUEST)
         request_body["password"] = PasswordHasher().hash(request_body["password"])
         # 현재 날씨 취득
-        response = requests.get(url = "http://api.weatherapi.com/v1/current.json?key=&q=seoul&aqi=no&lang=ko")
+        response = requests.get(url = "http://api.weatherapi.com/v1/current.json?key=958993396adc4c6ea8701154220609&q=seoul&aqi=no&lang=ko")
         response_body = json.loads(response.content)
         weather_condition = response_body["current"]["condition"]["text"]
         request_body["current_weather"] = weather_condition
@@ -72,3 +71,25 @@ def create_board(request):
             return Response({"message":"success"}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"message":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["POST"])
+def delete_board(request, id):
+    """
+    게시물을 삭제한다.
+    입력받은 password와 게시물의 password가 동일하면 삭제한다.
+
+    """
+    try:
+        password = request.data["password"]
+        board = get_object_or_404(Board, id=id)
+        # 패스워드  체크
+        try:
+            PasswordHasher().verify(board.password, password)
+        except Exception as e:
+            return Response({"message":"패스워드가 틀렸습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        # 삭제
+        board.delete()
+        return Response({"message":"success"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"message":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
